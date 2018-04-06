18dbeb82be6ba230c2c639e4d3a5bffa18176ec5
testprograms123 <pranavshriramalt@gmail.com>
2018-04-07 00:23:02 +0800

    telebot.py more fix

--- telegramBot.py
+++ telegramBot.py
@@ -46,27 +46,23 @@ def main():
     while True:
         greet_bot.get_updates(new_offset)
         last_update = greet_bot.get_last_update()
-        last_update_id = last_update['update_id']
-        last_chat_text = last_update['message']['text']
-        last_chat_id = last_update['message']['chat']['id']
-        last_chat_name = last_update['message']['chat']['first_name']
-        if last_chat_text.lower() in greetings and today == now.day \
-            and 6 <= hour < 12:
-            greet_bot.send_message(last_chat_id,
-                                   'Good Morning  {}'.format(last_chat_name))
+        print("LAST UPDATE: ", type(last_update), "\n", last_update)
+        
+        if type(last_update) != list:
+            last_update_id = last_update['update_id']
+            last_chat_text = last_update['message']['text']
+            last_chat_id = last_update['message']['chat']['id']
+            last_chat_name = last_update['message']['chat']['first_name']
+            if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
+                greet_bot.send_message(last_chat_id, 'Good Morning  {}'.format(last_chat_name))
             today += 1
-        elif last_chat_text.lower() in greetings and today == now.day \
-            and 12 <= hour < 17:
-            greet_bot.send_message(last_chat_id,
-                                   'Good Afternoon {}'.format(last_chat_name))
+        elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
+            greet_bot.send_message(last_chat_id, 'Good Afternoon {}'.format(last_chat_name))
             today += 1
-        elif last_chat_text.lower() in greetings and today == now.day \
-            and 17 <= hour < 23:
-            greet_bot.send_message(last_chat_id,
-                                   'Good Evening  {}'.format(last_chat_name))
+        elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
+            greet_bot.send_message(last_chat_id, 'Good Evening  {}'.format(last_chat_name))
             today += 1
         new_offset = last_update_id + 1
-	
 
 if __name__ == '__main__':
     try:
